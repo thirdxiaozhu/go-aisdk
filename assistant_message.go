1f16a631c07ffef96fc0dc6e572ab02e866343a9
liusuxian <382185882@qq.com>
2025-04-10 20:33:39 +0800

    更新代码

--- assistant_message.go
+++ assistant_message.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2025-04-09 17:39:52
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2025-04-10 13:35:40
+ * @LastEditTime: 2025-04-10 20:16:01
  * @Description:
  *
  * Copyright (c) 2025 by liusuxian email: 382185882@qq.com, All Rights Reserved.
@@ -13,14 +13,14 @@ import "encoding/json"
 
 // AssistantMessage 助手消息
 type AssistantMessage struct {
-	Audio             *ChatAssistantMsgAudio      `json:"audio,omitempty"`              // 音频
-	Content           string                      `json:"content,omitempty"`            // 文本内容
-	MultimodalContent []ChatAssistantMsgPart      `json:"multimodal_content,omitempty"` // 多模态内容
-	Name              string                      `json:"name,omitempty"`               // 参与者名称
-	Refusal           string                      `json:"refusal,omitempty"`            // 拒绝消息
-	ToolCalls         []ChatAssistantMsgToolCalls `json:"tool_calls,omitempty"`         // 工具调用
-	Prefix            bool                        `json:"prefix,omitempty"`             // (DeepSeek)设置此参数为 true，来强制模型在其回答中以此 assistant 消息中提供的前缀内容开始
-	ReasoningContent  string                      `json:"reasoning_content,omitempty"`  // (DeepSeek)用于 deepseek-reasoner 模型在对话前缀续写功能下，作为最后一条 assistant 思维链内容的输入。使用此功能时，prefix 参数必须设置为 true
+	Audio             *ChatAssistantMsgAudio `json:"audio,omitempty"`              // 音频
+	Content           string                 `json:"content,omitempty"`            // 文本内容
+	MultimodalContent []ChatAssistantMsgPart `json:"multimodal_content,omitempty"` // 多模态内容
+	Name              string                 `json:"name,omitempty"`               // 参与者名称
+	Refusal           string                 `json:"refusal,omitempty"`            // 拒绝消息
+	ToolCalls         []ToolCalls            `json:"tool_calls,omitempty"`         // 工具调用
+	Prefix            bool                   `json:"prefix,omitempty"`             // 设置此参数为 true，来强制模型在其回答中以此 assistant 消息中提供的前缀内容开始
+	ReasoningContent  string                 `json:"reasoning_content,omitempty"`  // 用于模型在对话前缀续写功能下，作为最后一条 assistant 思维链内容的输入。使用此功能时，prefix 参数必须设置为 true
 }
 
 // GetRole 获取消息角色
@@ -66,17 +66,4 @@ type ChatAssistantMsgPart struct {
 	Type    ChatAssistantMsgPartType `json:"type"`              // 内容类型
 	Text    string                   `json:"text,omitempty"`    // 文本内容
 	Refusal string                   `json:"refusal,omitempty"` // 拒绝消息
-}
-
-// ChatAssistantMsgToolFunction 函数
-type ChatAssistantMsgToolFunction struct {
-	Arguments string `json:"arguments"` // 函数参数
-	Name      string `json:"name"`      // 函数名
-}
-
-// ChatAssistantMsgToolCalls 工具调用
-type ChatAssistantMsgToolCalls struct {
-	Function *ChatAssistantMsgToolFunction `json:"function"` // 函数调用
-	ID       string                        `json:"id"`       // 工具ID
-	Type     ToolType                      `json:"type"`     // 工具类型
 }
--- chat_model.go
+++ chat_model.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2025-04-09 20:13:44
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2025-04-10 13:30:16
+ * @LastEditTime: 2025-04-10 20:17:13
  * @Description:
  *
  * Copyright (c) 2025 by liusuxian email: 382185882@qq.com, All Rights Reserved.
@@ -41,8 +41,8 @@ const (
 	ChatAudioVoiceTypeShimmer ChatAudioVoiceType = "shimmer"
 )
 
-// ChatAudio 音频输出
-type ChatAudio struct {
+// ChatAudioOutputArgs 音频输出参数
+type ChatAudioOutputArgs struct {
 	Format ChatAudioFormatType `json:"format"` // 输出音频格式
 	Voice  ChatAudioVoiceType  `json:"voice"`  // 模型回应时使用的声音
 }
@@ -192,7 +192,7 @@ type ChatWebSearchOptions struct {
 type ChatRequest struct {
 	BaseRequest
 	Messages            []ChatMessage           `json:"messages"`                        // 消息数组
-	Audio               *ChatAudio              `json:"audio,omitempty"`                 // 音频输出
+	Audio               *ChatAudioOutputArgs    `json:"audio,omitempty"`                 // 音频输出的参数
 	FrequencyPenalty    float32                 `json:"frequency_penalty,omitempty"`     // 介于 -2.0 和 2.0 之间的数值。正值会根据文本中已有内容的出现频率对新 token 进行惩罚，从而降低模型逐字重复相同内容的可能性
 	LogitBias           map[string]int          `json:"logit_bias,omitempty"`            // 修改指定标记在补全中出现的可能性
 	LogProbs            bool                    `json:"logprobs,omitempty"`              // 是否返回所输出 token 的对数概率
@@ -220,7 +220,104 @@ type ChatRequest struct {
 	WebSearchOptions    *ChatWebSearchOptions   `json:"web_search_options,omitempty"`    // 网络搜索选项
 }
 
+// ChatFinishReason 模型停止生成 token 的原因
+type ChatFinishReason string
+
+const (
+	ChatFinishReasonNull                       ChatFinishReason = "null"
+	ChatFinishReasonStop                       ChatFinishReason = "stop"                         // 模型自然停止生成，或遇到 stop 序列中列出的字符串
+	ChatFinishReasonLength                     ChatFinishReason = "length"                       // 输出长度达到了模型上下文长度限制，或达到了 max_completion_tokens 的限制
+	ChatFinishReasonContentFilter              ChatFinishReason = "content_filter"               // 输出内容因触发过滤策略而被过滤
+	ChatFinishReasonToolCalls                  ChatFinishReason = "tool_calls"                   // 模型调用了工具
+	ChatFinishReasonInsufficientSystemResource ChatFinishReason = "insufficient_system_resource" // 系统推理资源不足，生成被打断
+)
+
+// MarshalJSON 序列化JSON
+func (r ChatFinishReason) MarshalJSON() (b []byte, err error) {
+	if r == ChatFinishReasonNull || r == "" {
+		return []byte("null"), nil
+	}
+	return []byte(`"` + string(r) + `"`), nil
+}
+
+// ChatTopLogProbs 输出概率 top N 的 token 的列表，以及它们的对数概率
+type ChatTopLogProbs struct {
+	Bytes   []byte  `json:"bytes,omitempty"` // 一个包含该 token UTF-8 字节表示的整数列表。一般在一个 UTF-8 字符被拆分成多个 token 来表示时有用。如果 token 没有对应的字节表示，则该值为 null
+	LogProb float64 `json:"logprob"`         // 该 token 的对数概率
+	Token   string  `json:"token"`           // 输出的 token
+}
+
+// ChatLogProb 对数概率信息
+type ChatLogProb struct {
+	Bytes       []byte            `json:"bytes,omitempty"` // 一个包含该 token UTF-8 字节表示的整数列表。一般在一个 UTF-8 字符被拆分成多个 token 来表示时有用。如果 token 没有对应的字节表示，则该值为 null
+	LogProb     float64           `json:"logprob"`         // 该 token 的对数概率
+	Token       string            `json:"token"`           // 输出的 token
+	TopLogProbs []ChatTopLogProbs `json:"top_logprobs"`    // 一个包含在该输出位置上，输出概率 top N 的 token 的列表，以及它们的对数概率
+}
+
+// ChatLogProbs 对数概率信息
+type ChatLogProbs struct {
+	Content []ChatLogProb `json:"content,omitempty"` // 一个包含输出 token 对数概率信息的列表
+	Refusal []ChatLogProb `json:"refusal,omitempty"` // 一个带有对数概率信息的消息拒绝 token 列表
+}
+
+// ChatAudioOutput 音频响应数据
+type ChatAudioOutput struct {
+	Data       string `json:"data"`       // Base64 编码的音频字节，格式在请求中指定
+	ExpiresAt  int64  `json:"expires_at"` // 音频响应在服务器上不再可访问的 Unix 时间戳（秒）
+	ID         string `json:"id"`         // 音频响应的唯一标识符
+	Transcript string `json:"transcript"` // 模型生成的音频文本转录
+}
+
+// ChatAnnotationType 消息的注释类型
+type ChatAnnotationType string
+
+const (
+	ChatAnnotationTypeURLCitation ChatAnnotationType = "url_citation" // 网络搜索工具的 URL 引用
+)
+
+// ChatAnnotationURLCitation 网络搜索工具的 URL 引用
+type ChatAnnotationURLCitation struct {
+	EndIndex   int    `json:"end_index"`   // 消息中URL引用的最后一个字符的索引
+	StartIndex int    `json:"start_index"` // 消息中URL引用的第一个字符的索引
+	Title      string `json:"title"`       // 网络资源的标题
+	URL        string `json:"url"`         // 网络资源的URL
+}
+
+// ChatAnnotation 消息的注释
+type ChatAnnotation struct {
+	Type        ChatAnnotationType        `json:"type"`         // 注释的类型
+	URLCitation ChatAnnotationURLCitation `json:"url_citation"` // 网络搜索工具的 URL 引用
+}
+
+// ChatCompletionMessage 模型生成的 completion 消息
+type ChatCompletionMessage struct {
+	Content          string           `json:"content,omitempty"`           // 文本内容
+	ReasoningContent string           `json:"reasoning_content,omitempty"` // 推理内容
+	Refusal          string           `json:"refusal,omitempty"`           // 拒绝消息
+	Role             string           `json:"role"`                        // 角色
+	Annotations      []ChatAnnotation `json:"annotations,omitempty"`       // 消息的注释，在适用情况下提供，例如使用网络搜索工具时
+	Audio            *ChatAudioOutput `json:"audio,omitempty"`             // 音频响应数据
+	ToolCalls        []ToolCalls      `json:"tool_calls"`                  // 工具调用
+}
+
+// ChatChoice 模型生成的 completion
+type ChatChoice struct {
+	FinishReason ChatFinishReason      `json:"finish_reason"`      // 模型停止生成 token 的原因
+	Index        int                   `json:"index"`              // 该 completion 在模型生成的 completion 的选择列表中的索引
+	LogProbs     *ChatLogProbs         `json:"logprobs,omitempty"` // 该 choice 的对数概率信息
+	Message      ChatCompletionMessage `json:"message"`            // 模型生成的 completion 消息
+}
+
 // ChatResponse 聊天响应
 type ChatResponse struct {
 	BaseResponse
+	Choices           []ChatChoice `json:"choices"`                // 模型生成的 completion 的选择列表
+	Created           int64        `json:"created"`                // 创建聊天完成时的 Unix 时间戳（以秒为单位）
+	ID                string       `json:"id"`                     // 该对话的唯一标识符
+	Model             string       `json:"model"`                  // 生成该 completion 的模型名
+	Object            string       `json:"object"`                 // 对象的类型
+	ServiceTier       string       `json:"service_tier,omitempty"` // 用于处理请求的服务层级
+	SystemFingerprint string       `json:"system_fingerprint"`     // 此指纹表示模型运行的后端配置。可以与 seed 请求参数一起使用，以了解何时进行了可能影响确定性的后端更改
+	Usage             Usage        `json:"usage"`                  // 该对话补全请求的用量信息
 }
